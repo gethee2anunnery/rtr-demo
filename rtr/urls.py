edd032d7eb35db1ed770e1edee062f96fedc3d69
Sara Perry <paraserry@gmail.com>
2017-02-23 15:31:06 -0500

    adding static files to url conf

--- rtr/urls.py
+++ rtr/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.conf import settings
 
 
 
